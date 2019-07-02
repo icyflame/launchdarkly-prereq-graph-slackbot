30a685127f332013b34758deb117c67ca00b2c05
Siddharth Kannan <mail@siddharthkannan.in>
2019-07-02 18:44:59 +0900

    Include the off variation with name and value in the node label

--- gen_digraph.js
+++ gen_digraph.js
@@ -6,8 +6,17 @@ const {
 const gen_digraph = ({ flag_items_with_prereqs, flat_mapping }) => {
     const g = graphviz.digraph('prerequisites');
 
-    flag_items_with_prereqs.forEach(({ key, environments }) => {
-        g.addNode(transform_flag_name(key), {
+    flag_items_with_prereqs.forEach(({ variations, key, environments }) => {
+        const offVariation = variations.filter(variation => variation.value === environments.production.offVariation)
+        const flagName = transform_flag_name(key);
+        const boxLabel = [ flagName, '' ];
+
+        if (offVariation.length >= 1) {
+            boxLabel.push(`Off Variation = ${offVariation[0].value}, ${offVariation[0].name}`)
+        }
+
+        g.addNode(flagName, {
+            label: boxLabel.join('\n'),
             shape: 'box',
             color: environments.production.on ? '#BAFFB0' : '#FF818F',
             style: 'filled',
